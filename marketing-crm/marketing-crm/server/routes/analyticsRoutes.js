8bb1a06efee3cd57da60bd899bd7339789dc1cb2
Shanmathi <shanmathi030303@gmail.com>
2026-03-30 16:00:58 +0530

    Delete marketing-crm/marketing-crm directory

--- marketing-crm/marketing-crm/server/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats, trackEvent } = require('../controllers/analyticsController');
-const { protect } = require('../middleware/auth');
-
-router.get('/dashboard', protect, getDashboardStats);
-router.post('/track', trackEvent);
-
-module.exports = router;
